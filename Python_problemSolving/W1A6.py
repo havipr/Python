cd5c65c08945f124bf05582834d61a1e10bc50f8
Javi <javierreinapinzon@gmail.com>
2022-06-30 13:06:47 +1000

    solved and updated W1A6 exercise

--- Python_problemSolving/W1A6.py
+++ Python_problemSolving/W1A6.py
@@ -1,3 +1,4 @@
+from operator import ne
 from PyTest import *
 ##//////////////////////////// PROBLEM STATEMENT //////////////////////////////
 ## Given a string, print a version without the first 2 chars. Except keep    //
@@ -7,3 +8,22 @@ from PyTest import *
 ##   "java" -> "va"                                                          //
 ##   "away" -> "aay"                                                         //
 ##/////////////////////////////////////////////////////////////////////////////
+
+#We ask the user for an input 
+a = input('Pls insert an input: ')
+
+#We convert the input into a list to check each item 
+newa = a.split()
+
+#iterate the len of the list 
+for i in range(len(newa)):
+    #if the first caracter is a
+    if newa[0][0] == 'a':
+        print(newa[0][0], newa[0][2:])
+    #if the second caracter is b   
+    elif newa[0][0] == 'b':
+        print(newa[1],newa[0][2:])
+    #if the input dosent have any a or b at in the index 0 
+    else:
+        print(newa[0][2:])
+
