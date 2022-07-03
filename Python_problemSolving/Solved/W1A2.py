fd361f5f5d03552bea6cc5319eceda9cca5dda8d
Javi <javierreinapinzon@gmail.com>
2022-07-03 21:19:30 +1000

    solved an updated W1A2 exercise

--- Python_problemSolving/Solved/W1A2.py
+++ Python_problemSolving/Solved/W1A2.py
@@ -7,3 +7,20 @@ from PyTest import *
 ##   "xbadxx" -> True                                                        //
 ##   "xxbadxx" -> False                                                      //
 ##/////////////////////////////////////////////////////////////////////////////
+
+#Ask for user input 
+a = input('pls type a word: ').lower()
+#convert the input into a list 
+lst = a.split()
+
+#iterate the list 
+for i in lst:
+    #We slice it to check the first 3 characters
+    if i[0:3] == 'bad':
+        print(True)
+    #We slice it to check the first 3 characters
+    elif i[1:4] == 'bad':
+        print(True)
+    #if none are True print False 
+    else:
+        print(False)
