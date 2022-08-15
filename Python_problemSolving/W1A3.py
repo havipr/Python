9d276addaebd1f7051eefde6173672fe410f0fc0
Javi <javierreinap@protonmail.com>
2022-08-15 18:30:20 +1000

    finish exercise W1A3

--- Python_problemSolving/W1A3.py
+++ Python_problemSolving/W1A3.py
@@ -7,21 +7,27 @@ from PyTest import *
 ##   "1cat1cadodog" -> True                                                  //
 ##/////////////////////////////////////////////////////////////////////////////
 
-word = input('type the word: ')
-countCat = []
-countDog = []
 
-while True:
-    cat = word[:3]
-    if cat == 'cat':
-        countCat.append(cat)
-    dog = word[3:]
-    if dog == 'dog':
-        countDog.append(dog)
-    break
+word = input('type the word: ')
+countCat = 0
+countDog = 0
+
+for i in range(len(word) - 2):
+    if word[i] == 'c':
+        if word[i+1] == 'a':
+            if word[i+2] == 't':
+                countCat += 1
+
+for j in range(len(word) - 2):
+    if word[j] == 'd':
+        if word[j+1] == 'o':
+            if word[j+2] == 'g':
+                countDog += 1
 
 
 if countCat == countDog:
     print(True)
+else:
+    print(False)
 
     
