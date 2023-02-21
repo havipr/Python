dbdbd69f1f85af049b4b8cf3d1cb36e95cc785e2
Javi <66537664+javiSYD@users.noreply.github.com>
2023-02-21 12:28:43 +1000

    Update W173.py
    Done exercise w173 in python

--- Python_Practice/W173.py
+++ Python_Practice/W173.py
@@ -6,3 +6,10 @@ from PyTest import *
 ##   10 -> 55                                 //
 ##   20 -> 210                                //
 ##//////////////////////////////////////////////
+
+
+n = int(input("Enter a number: "))
+sum = 0
+for i in range(n+1):
+    sum += i ** 3
+print("Sum of cubes of numbers from 0 to", n, "is:", sum)
