c83228ba05c2da46dd19197ce468b41c9141b198
Javi <66537664+javiSYD@users.noreply.github.com>
2023-02-22 11:12:20 +1000

    Update W172.py
    Solved exercise W172 in python

--- Python_Practice/W172.py
+++ Python_Practice/W172.py
@@ -2,3 +2,7 @@ from PyTest import *
 ##//////////////// PROBLEM STATEMENT /////////////////
 ## Print the numbers from 10 to 20 using a for loop //
 ##////////////////////////////////////////////////////
+
+
+for i in range(10,20):
+  print ('This is a super basic exercise but here is the answer: 'i)
