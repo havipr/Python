df44991d20d030bd3879b5e3705ab1f14655ac56
Javi <66537664+javiSYD@users.noreply.github.com>
2023-03-01 15:59:20 +1000

    Update W1K6.py
    Practice W1K6 in Python

--- Python_Practice/W1K6.py
+++ Python_Practice/W1K6.py
@@ -8,3 +8,16 @@ from PyTest import *
 ##   "xxxabyyyycd" 3 -> 3                                                    //
 ##   "a" 2 -> 0                                                              //
 ##/////////////////////////////////////////////////////////////////////////////
+
+
+def count_nsequences(string, n):
+    count = 0
+    for i in range(len(string)-n+1):
+        if string[i:i+n] == string[i]*n:
+            count += 1
+    return count
+
+# Example usage:
+print(count_nsequences("abcXXXabc", 3))   
+print(count_nsequences("xxxabyyyycd", 3)) 
+print(count_nsequences("a", 2))          
