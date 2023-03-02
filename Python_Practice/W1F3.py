598edc833b882c129808a8d3eb059c0e8bde1c19
Javi <66537664+javiSYD@users.noreply.github.com>
2023-03-02 12:18:09 +1000

    Update W1F3.py
    fixed problem W1F3

--- Python_Practice/W1F3.py
+++ Python_Practice/W1F3.py
@@ -6,3 +6,14 @@ from PyTest import *
 ##    4, 4, 1  -> True                                                       //
 ##    4, 4, 1, 2, 2  -> False                                                //
 ##/////////////////////////////////////////////////////////////////////////////
+
+
+def prob1F3(nums):
+    has_22 = False
+    has_44 = False
+    for i in range(len(nums) - 1):
+        if nums[i] == 2 and nums[i+1] == 2:
+            has_22 = True
+        elif nums[i] == 4 and nums[i+1] == 4:
+            has_44 = True
+    return has_22 != has_44
