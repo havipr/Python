103f9a67013d43813672b4962f1bbcc9c5b970ed
Javi <66537664+javiSYD@users.noreply.github.com>
2023-03-06 14:37:11 +1000

    Update W1C4.py
    Practiced with exercise W1C4 in python

--- Python_Practice/W1C4.py
+++ Python_Practice/W1C4.py
@@ -7,3 +7,11 @@ from PyTest import *
 ##    1, 2, 3    7, 3, 2  -> False                                           //
 ##    1, 2, 3    1, 3  -> True                                               //
 ##/////////////////////////////////////////////////////////////////////////////
+
+
+
+def same_first_last(a, b):
+    if a[0] == b[0] or a[-1] == b[-1]:
+        return True
+    else:
+        return False
